6f1a8ccd0bbca75a6dbfbfa1c3528b4ef69bf0eb
powel743 <powelcollin40@gmail.com>
2026-05-24 22:20:05 +0300

    fix: remove ws from supabase config

--- apps/api/src/config/supabase.ts
+++ apps/api/src/config/supabase.ts
@@ -1,10 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import { env } from './env'
-import ws from 'ws'
 
 export const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY, {
   auth: { autoRefreshToken: false, persistSession: false },
-  realtime: { transport: ws },
 })
 
 export const supabaseAdmin = createClient(
@@ -12,6 +10,5 @@ export const supabaseAdmin = createClient(
   env.SUPABASE_SERVICE_ROLE_KEY,
   {
     auth: { autoRefreshToken: false, persistSession: false },
-    realtime: { transport: ws },
   }
 )
